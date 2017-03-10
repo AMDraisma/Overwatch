4ccfbf36a371cc5a8847382042d5b13f249c1010
ArjanDraisma <adraisma@gmail.com>
2017-03-10 19:10:36 +0100

    Gulp now works. I think.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,14 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var minify = require('gulp-minify');
 var inject = require('gulp-inject');
+var typescript = require('gulp-tsc');
 
 var wwwroot = './wwwroot'
 var libs = '{bootstrap,jquery}'
 
 // npm dependencies
 gulp.task('npm:copy', (cb) => {
-    gulp.src([`node_modules/${libs}/dist/*`], {base: 'node_modules'})
+    gulp.src([`node_modules/${libs}/dist/*`, `node_modules/${libs}/dist/**/*`], {base: 'node_modules'})
         .pipe(gulp.dest(`${wwwroot}/lib/`));
     cb();
 });
@@ -26,29 +27,40 @@ gulp.task('ow:copy', (cb) => {
     cb();
 });
 
-// inject dependencies
-gulp.task('npm:insertfiles', function(cb){
-    return gulp.src(`${wwwroot}/index.html`) //file with tags for injection
-        .pipe(inject(gulp.src([`${wwwroot}/lib/**/*.min.js`, `.${wwwroot}/lib/**/*.css`], {read: false}), {
-                starttag: '<!-- npm:{{ext}} -->',
-                endtag: '<!-- endnpm -->',
-                relative:true
-            }
-        ))
-        .pipe(gulp.dest('./src')); //where index.html will be saved. Same dir for overwrite old one
+
+// compile typescript
+gulp.task('tsc', (cb) => {
+    gulp.src([`${wwwroot}/js/*.ts`])
+        .pipe(typescript())
+        .pipe(gulp.dest(`${wwwroot}/js`))
+    cb();
 });
 
-gulp.task('ow:insertfiles', function(cb){
+// inject dependencies
+gulp.task('insertjs', function(cb){
     return gulp.src(`${wwwroot}/index.html`) //file with tags for injection
-        .pipe(inject(gulp.src([`${wwwroot}/js/*.js`], {read: false}), {
-                starttag: '<!-- ow:{{ext}} -->',
-                endtag: '<!-- endow -->',
+        .pipe(inject(gulp.src([`${wwwroot}/lib/*/**/*.min.js`, `${wwwroot}/js/*.js`], {base: wwwroot}), {
+                starttag: '<!-- gulp:js -->',
+                endtag: '<!-- endgulp -->',
                 relative:true
             }
         ))
-        .pipe(gulp.dest('./src')); //where index.html will be saved. Same dir for overwrite old one
+        .pipe(gulp.dest(wwwroot)); //where index.html will be saved. Same dir for overwrite old one
+    cb();
+});
+
+gulp.task('insertcss', function(cb){
+    return gulp.src(`${wwwroot}/index.html`) //file with tags for injection
+        .pipe(inject(gulp.src([`${wwwroot}/lib/*/**/*.css`], {base: wwwroot}), {
+                starttag: '<!-- gulp:css -->',
+                endtag: '<!-- endgulp -->',
+                relative:true
+            }
+        ))
+        .pipe(gulp.dest(wwwroot)); //where index.html will be saved. Same dir for overwrite old one
+    cb();
 });
 
 gulp.task('default', () => {
-    runSequence('npm:copy', 'ow:copy', 'npm:insertfiles');
+    runSequence('npm:copy', 'ow:copy', 'tsc', 'insertjs', 'insertcss');
 });

5bd0286c9fee111b65f3922e0f6f4c644ceb6773
CrudeOil <adraisma@gmail.com>
2017-03-10 17:27:03 +0100

    use gulp they said. it will be fun they said.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,57 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
+var runSequence = require('run-sequence');
+var minify = require('gulp-minify');
 
-gulp.task('tsc', () => {
-    
+var wwwroot = "./wwwroot"
+
+
+// npm dependencies
+gulp.task("npm:copy", (cb) => {
+    gulp.src(["node_modules/*/dist/**/*"], {base: "node_modules"})
+        .pipe(gulp.dest(`.${wwwroot}/lib/`));
+    cb();
 });
+
+
+// overwatch stuff
+gulp.task("ow:copy", (cb) => {
+    gulp.src(["src/*.html"], {base: "src"})
+        .pipe(gulp.dest(`.${wwwroot}/`));
+    gulp.src(["src/js/*.js"], {base: "src/js"})
+        //.pipe(minify())
+        .pipe(gulp.dest(`.${wwwroot}/js`));
+    gulp.src(["src/config/*"], {base: "src/config"})
+        .pipe(gulp.dest(`.${wwwroot}/config`));
+    cb();
+});
+
+// inject dependencies
+gulp.task('npm:insertfiles', function(cb){
+    return gulp.src(`.${wwwroot}/index.html`) //file with tags for injection
+        .pipe(inject(gulp.src([`.${wwwroot}/lib/**/*.min.js`, `.${wwwroot}/lib/*.css`], {read: false}), {
+                starttag: '<!-- npm:{{ext}} -->',
+                endtag: '<!-- endnpm -->',
+                relative:true
+            }
+        ))
+        .pipe(gulp.dest('./src')); //where index.html will be saved. Same dir for overwrite old one
+})
+gulp.task('ow:insertfiles', function(cb){
+    return gulp.src(`.${wwwroot}/index.html`) //file with tags for injection
+        .pipe(inject(gulp.src([`.${wwwroot}/js/*.js`], {read: false}), {
+                starttag: '<!-- ow:{{ext}} -->',
+                endtag: '<!-- endow -->',
+                relative:true
+            }
+        ))
+        .pipe(gulp.dest('./src')); //where index.html will be saved. Same dir for overwrite old one
+})
+
+
+gulp.task('npm:buildlib', (callback) => {
+    runSequence('npm:copy', 'ow:copy', callback);
+})
+
+gulp.task('default', () => {
+    runSequence('npm:buildlib');
+})

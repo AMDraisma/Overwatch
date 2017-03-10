980953c5e696bd751ec6343f9dd8cc7d2bd01b3b
CrudeOil <adraisma@gmail.com>
2017-03-10 10:56:58 +0100

    complying to my own tslint rules

--- js/quiz.ts
+++ js/quiz.ts
@@ -19,13 +19,13 @@ namespace quiz{
 
     export class GameMaster {
         public static GenerateQuestion(heroes: IHero[], categories: ICategory[]): Question {
-            let q: Question = new Question;
+            let q: Question = new Question();
 
             q.hero = GameMaster.PickRandom<IHero>(heroes);
             let category: ICategory = GameMaster.PickRandom<ICategory>(categories);
             q.attribute = category.fullName;
             // TODO: neat, but need what this is an answer to
-            q.answer = GameMaster.PickAnswerFromPath(q.hero, category.path);
+            q.answer = GameMaster.PickFromPath(q.hero, category.path);
             console.log(q);
             return q;
         }
@@ -34,16 +34,16 @@ namespace quiz{
             return arr[Math.floor(Math.random()*arr.length)];
         }
 
-        private static PickAnswerFromPath(obj: any, path: string): {[value: string]: string} {
+        private static PickFromPath(obj: any, path: string, attribute: boolean = false): {[value: string]: string} {
             let splitpath: string[] = [];
             let i: number = path.indexOf('/');
 
             if (i > 0) {
                 splitpath[0] = path.substr(0, i);
                 splitpath[1] = path.substr(i+1);
-                
+
                 if (splitpath[0] in obj) {
-                    return GameMaster.PickAnswerFromPath(obj[splitpath[0]], splitpath[1]);
+                    return GameMaster.PickFromPath(obj[splitpath[0]], splitpath[1]);
                 }
                 if (splitpath[0] === '*') {
                     let subSelection: any
@@ -51,14 +51,14 @@ namespace quiz{
                     // TODO: dear god think of something better
                     while (answer === undefined) {
                         subSelection = GameMaster.PickRandom<any>(obj)
-                        answer = GameMaster.PickAnswerFromPath(subSelection, splitpath[1]);
+                        answer = GameMaster.PickFromPath(subSelection, splitpath[1]);
                     }
                     return answer;
                 }
-                if (splitpath[0].indexOf('|') != -1) {
+                if (splitpath[0].indexOf('|') !== -1) {
                     let choice = GameMaster.PickRandom<string>(splitpath[0].split('|'))
                     let subSelection = obj[choice];
-                    return GameMaster.PickAnswerFromPath(subSelection, splitpath[1]);
+                    return GameMaster.PickFromPath(subSelection, splitpath[1]);
                 }
             }else{
                 return obj[path];
